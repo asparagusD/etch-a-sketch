126103315f8e50e4397ddc9f382db6f20047f56f
asparagusD <nufel1130@gmail.com>
2025-01-27 12:34:14 +0600

    Finish second checkpoint of the project
    Added hover effect which changes color of the squares when hovered over and a button to change the size of the grid

--- bin.js
+++ bin.js
@@ -1,18 +1,46 @@
 const bigDiv = document.querySelector("#daddy-container");
 let div = [];
+// let subDiv;
+let numberOfSquares;
+let cngNumberOfSquares = document.createElement("button");
+cngNumberOfSquares.textContent = "Change number of squares";
+bigDiv.appendChild(cngNumberOfSquares);
+cngNumberOfSquares.addEventListener("click", () => {
+    numberOfSquares = +prompt("How many squares do you need per side, boy?", "");
+    // grid(numberOfSquares);
+});
 
-for (let i=0; i<16; i++) {
-    div[i] = document.createElement("div");
-    div[i].setAttribute("id", `row${i}`);
-    div[i].style.cssText = "flex: 1; display: flex;";
-    bigDiv.appendChild(div[i]);
-    
-    for (let j=0; j<16; j++) {
-        const subDiv = document.createElement("div");
-        subDiv.setAttribute("class", "column");
-        div[i].appendChild(subDiv);
-    }
+function grid(squares) {
+    for (let i=0; i<squares; i++) {
+        div[i] = document.createElement("div");
+        div[i].setAttribute("id", `row${i}`);
+        div[i].style.cssText = "flex: 1; display: flex;";
+        bigDiv.appendChild(div[i]);
         
-
+        for (let j=0; j<squares; j++) {
+            const subDiv = document.createElement("div");
+            subDiv.setAttribute("class", "column");
+            div[i].appendChild(subDiv);
+    
+            subDiv.addEventListener("mouseenter", () => {
+                subDiv.style.cssText = "background-color: beige;";
+            });
+            subDiv.addEventListener("mouseleave", () => {
+                subDiv.style.cssText = "background-color: cyan;";
+            });
+        }
+    }
 }
+
+// function clearGrid(removeElements) {
+//     for (let i=0; i<removeElements; i++) {
+//         bigDiv.removeChild(div[i]);
+
+
+//     }
+// }
+
+grid(16);
+
+
 
